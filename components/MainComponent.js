3e35b01717e51f40d618d684cef4f3145d886a38
Tan Wee Han <twh@live.com.sg>
2019-01-23 15:52:01 +0800

    React Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { View, Platform } from 'react-native';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createStackNavigator, createAppContainer, createDrawerNavigator } from 'react-navigation';
 
 const MenuNavigator = createStackNavigator({
     Menu: { screen: Menu },
@@ -21,6 +22,40 @@ const MenuNavigator = createStackNavigator({
     }
 );
 
-AppContainer = createAppContainer(MenuNavigator);
+const HomeNavigator = createStackNavigator({
+    Home: { screen: Home }
+    }, {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#512DA8'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator({
+    Home: {
+        screen: HomeNavigator,
+        navigationOptions: {
+            title: 'Home',
+            drawerLabel: 'Home'
+        }
+    },
+    Menu: {
+        screen: MenuNavigator,
+        navigationOptions: {
+            title: 'Menu',
+            drawerLabel: 'Menu'
+        }
+    }
+}, {
+    drawerBackgroundColor: '#D1C4E9'
+})
+
+AppContainer = createAppContainer(MainNavigator);
 
 export default AppContainer;
