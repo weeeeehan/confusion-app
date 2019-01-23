6cc89da10e12a2da73f7e1af07aafcc4fac92d3c
Tan Wee Han <twh@live.com.sg>
2019-01-23 11:19:45 +0800

    Components Part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, FlatList } from 'react-native';
-import { ListItem, Avatar } from 'react-native-elements';
+import { ListItem } from 'react-native-elements';
 
 function Menu(props) {
 
@@ -11,8 +11,9 @@ function Menu(props) {
                 title={item.name}
                 subtitle={item.description}
                 hideChevron={true}
+                onPress={() => props.onPress(item.id)}
                 avatar={require('./images/uthappizza.png')}
-            />
+                />
         );
     }
 
@@ -21,7 +22,7 @@ function Menu(props) {
             data={props.dishes} 
             renderItem={renderMenuItem}
             keyExtractor={item => item.id.toString()}
-        />
+            />
     );
 }
 
