9559e9ba24c0688d3e1fa0dd8aaff8d35e02c467
Tan Wee Han <twh@live.com.sg>
2019-01-24 15:37:18 +0800

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, FlatList, ScrollView } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -54,19 +55,41 @@ class About extends Component {
                 </Card>
             )
         }
-        
-        return(
-            <ScrollView>
-                <History />
-                <Card title='Corporate Leadership'>
-                    <FlatList 
-                        data={this.props.leaders.leaders} 
-                        renderItem={renderLeaderItem}
-                        keyExtractor={item => item.id.toString()}
-                    />
-                </Card>
-            </ScrollView>
-        )
+
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card title='Corporate Leadership'>
+                        <FlatList 
+                            data={this.props.leaders.leaders} 
+                            renderItem={renderLeaderItem}
+                            keyExtractor={item => item.id.toString()}
+                        />
+                    </Card>
+                </ScrollView>
+            )
+        }   
     }
 }
 
