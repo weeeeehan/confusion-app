22228a3a9462ef7e3ff80b7b56055a0995fb2f80
Tan Wee Han <twh@live.com.sg>
2019-02-11 09:46:25 +0800

    Animatable

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
 
@@ -10,15 +11,19 @@ class Contact extends Component {
 
     render() {
         return(
-            <Card
-                title='Contact Infomation'>
-                <Text>121, Clear Water Bay Road</Text>
-                <Text>Clear Water Bay, Kowloon</Text>
-                <Text>HONG KONG</Text>
-                <Text>Tel: +852 1234 5678</Text>
-                <Text>Fax: +852 8765 4321</Text>
-                <Text>Email:confusion@food.net</Text>
-            </Card>
+            <ScrollView>
+                <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+                    <Card
+                        title='Contact Infomation'>
+                        <Text>121, Clear Water Bay Road</Text>
+                        <Text>Clear Water Bay, Kowloon</Text>
+                        <Text>HONG KONG</Text>
+                        <Text>Tel: +852 1234 5678</Text>
+                        <Text>Fax: +852 8765 4321</Text>
+                        <Text>Email:confusion@food.net</Text>
+                    </Card>
+                </Animatable.View>
+            </ScrollView>
         )
     }
 }
