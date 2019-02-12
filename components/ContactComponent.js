ca66947f99d59c3188405fc31bfa8950b03214fe
Tan Wee Han <twh@live.com.sg>
2019-02-12 10:47:06 +0800

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import { MailComposer } from 'expo';
 
 class Contact extends Component {
 
     static navigationOptions = {
         title: 'Contact Us'
+    }
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        });
     }
 
     render() {
@@ -15,12 +24,18 @@ class Contact extends Component {
                 <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
                     <Card
                         title='Contact Infomation'>
-                        <Text>121, Clear Water Bay Road</Text>
-                        <Text>Clear Water Bay, Kowloon</Text>
-                        <Text>HONG KONG</Text>
-                        <Text>Tel: +852 1234 5678</Text>
-                        <Text>Fax: +852 8765 4321</Text>
-                        <Text>Email:confusion@food.net</Text>
+                        <Text style={{margin: 10}}>121, Clear Water Bay Road</Text>
+                        <Text style={{margin: 10}}>Clear Water Bay, Kowloon</Text>
+                        <Text style={{margin: 10}}>HONG KONG</Text>
+                        <Text style={{margin: 10}}>Tel: +852 1234 5678</Text>
+                        <Text style={{margin: 10}}>Fax: +852 8765 4321</Text>
+                        <Text style={{margin: 10}}>Email:confusion@food.net</Text>
+                        <Button 
+                            title='Send Email'
+                            buttonStyle={{ backgroundColor: '#512DA8' }}
+                            icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                            onPress={this.sendMail}
+                        />
                     </Card>
                 </Animatable.View>
             </ScrollView>
